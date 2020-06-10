e1fec3f08b0ac7cf8acbd89aef87850757310cd5
gattlin1 <gattlin1@live.missouristate.edu>
2020-06-09 20:10:11 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    ForecastComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
